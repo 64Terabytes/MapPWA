309742e1e84e0c3b5dc9271ff9208c142559fa81
64Terabytes <jamin.pepper15@gmail.com>
2023-01-26 20:46:02 +1100

    Implemented Basic Map

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-
-document.addEventListener("DOMContentLoaded", showCoffees);
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
@@ -9,3 +6,9 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFtaW42OSIsImEiOiJjazh4c3ZzbzMwNHQ3M25xbjY1ZTJuMXc1In0.VWbwy4U7keq8T2O73GLUMw';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jamin69/clbvs8pg6000814pidp2st7ys'
+  });
